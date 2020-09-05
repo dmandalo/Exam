d9b2f56b2fa81186cb02803fca8b1d6880f840e9
Dayne Mandalore <dmandalo@oa-e3.msk.21-school.ru>
2020-09-05 14:00:28 +0300

    fix

--- lvl02/alpha_mirror.c
+++ lvl02/alpha_mirror.c
@@ -6,7 +6,7 @@
 /*   By: dmandalo <dmandalo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/06 15:59:51 by dmandalo          #+#    #+#             */
-/*   Updated: 2019/11/06 15:59:54 by dmandalo         ###   ########.fr       */
+/*   Updated: 2020/08/28 17:46:46 by dmandalo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,19 +15,19 @@
 int		main(int argc, char *argv[])
 {
 	int		i;
-	char	ltr;
+	char	tmp;
 
 	i = 0;
 	if (argc == 2)
 	{
 		while (argv[1][i])
 		{
-			ltr = argv[1][i];
+			tmp = argv[1][i];
 			if ('A' <= argv[1][i] && 'Z' >= argv[1][i])
-				ltr = 'Z' - argv[1][i] + 'A';
+				tmp = 'Z' - argv[1][i] + 'A';
 			if ('a' <= argv[1][i] && 'z' >= argv[1][i])
-			   	ltr = 'z' - argv[1][i] + 'a';
-			write(1, &ltr, 1);
+			   	tmp = 'z' - argv[1][i] + 'a';
+			write(1, &tmp, 1);
 			i += 1;
 		}
 	}
