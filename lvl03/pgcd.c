227e05aa4426fe8eaad44ca1b4d4be20c4ca5ff0
Dayne Mandalore <dmandalo@il-d1.msk.21-school.ru>
2020-09-07 18:46:15 +0300

    f

--- lvl03/pgcd.c
+++ lvl03/pgcd.c
@@ -6,7 +6,7 @@
 /*   By: dmandalo <dmandalo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/07 17:10:59 by dmandalo          #+#    #+#             */
-/*   Updated: 2020/09/07 17:15:27 by dmandalo         ###   ########.fr       */
+/*   Updated: 2020/09/07 18:45:40 by dmandalo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,21 +15,21 @@
 
 int		main(int argc, char const *argv[])
 {
-	int		nbr1;
-	int		nbr2;
+	int		a;
+	int		b;
 
 	if (argc == 3)
 	{
-		if ((nbr1 = atoi(argv[1])) > 0 && (nbr2 = atoi(argv[2])) > 0)
+		if ((a = atoi(argv[1])) > 0 && (b = atoi(argv[2])) > 0)
 		{
-			while (nbr1 != nbr2)
+			while (a != b)
 			{
-				if (nbr1 > nbr2)
-					nbr1 -= nbr2;
+				if (a > b)
+					a -= b;
 				else
-					nbr2 -= nbr1;
+					b -= a;
 			}
-			printf("%d", nbr1);
+			printf("%d", a);
 		}
 	}
 	printf("\n");
