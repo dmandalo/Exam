6a50f80071db9ad6e7f281b26f1bb6c9e9931f24
Dayne Mandalore <dmandalo@il-k5.msk.21-school.ru>
2020-09-11 15:17:01 +0300

    2argv

---
+++ lvl04/ft_itoa_base.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_itoa_base.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dmandalo <dmandalo@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/09/11 15:04:18 by dmandalo          #+#    #+#             */
+/*   Updated: 2020/09/11 15:16:13 by dmandalo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+char	*ft_itoa_base(int value, int base)
+{
+	char	*s;
+	long	n;
+	int		sign;
+	int		i;
+
+	n = (value < 0) ? -(long)value : value;
+	sign = (value < 0 && base == 10) ? -1 : 0;
+	i = (sign == -1) ? 2 : 1;
+	while ((n /= base) >= 1)
+		i++;
+	s = (char*)malloc(sizeof(char) * (i + 1));
+	s[i] = '\0';
+	n = (value < 0) ? -(long)value : value;
+	while (i-- + sign)
+	{
+		s[i] = (n % base < 10) ? n % base + '0' : n % base + 'A' - 10;
+		n /= base;
+	}
+	(i == 0) ? s[i] = '-' : 0;
+	return (s);
+}
