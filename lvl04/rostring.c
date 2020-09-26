fc642de99c648aa5eaf91280e26d05b76482c86b
Dayne Mandalore <dmandalo@am-g2.msk.21-school.ru>
2020-09-26 14:58:00 +0300

    fix

--- lvl04/rostring.c
+++ lvl04/rostring.c
@@ -6,41 +6,47 @@
 /*   By: dmandalo <dmandalo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/10 17:28:58 by dmandalo          #+#    #+#             */
-/*   Updated: 2020/09/10 17:29:42 by dmandalo         ###   ########.fr       */
+/*   Updated: 2020/09/26 14:57:00 by dmandalo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-int		main(int argc, char *argv[])
+int main (int argc , char **argv)
 {
-	int		i;
-	int		j;
-	int		tmp;
+	int i;
+	int j;
 
-	if (argc > 1)
+    i = 0;
+	if (argc >= 2)
 	{
-		i = 0;
-		j = 0;
-		while (argv[1][i] == ' ' || argv[1][i] == '\t')
-			i += 1;
-		tmp = i;
-		while (argv[1][i] && argv[1][i] != ' ' && argv[1][i] != '\t')
-			i += 1;
-		while (argv[1][i])
-		{
-			if (argv[1][i] && (argv[1][i] != ' ' && argv[1][i] != '\t') \
-				&& (argv[1][i - 1] == ' ' || argv[1][i - 1] == '\t'))
+		
+			while (argv[1][i] && (argv[1][i] == ' ' || argv[1][i] == '\t'))
+			i++;
+			j = i;
+			while (argv[1][i])
 			{
 				while (argv[1][i] && (argv[1][i] != ' ' && argv[1][i] != '\t'))
-					write(1, &argv[1][i++], 1);
-				write(1, " ", 1);
+					i++;
+				while (argv[1][i] && (argv[1][i] == ' ' || argv[1][i] == '\t'))
+					i++;
+				while (argv[1][i] && (argv[1][i] != ' ' && argv[1][i] != '\t') && (argv[1][i -1] == ' ' || argv[1][i - 1] == '\t'))
+				{
+					while (argv[1][i] && (argv[1][i] != ' ' && argv[1][i] != '\t'))
+					{
+						write (1, &argv[1][i], 1);
+						i++;
+					}
+					write (1, " ", 1);
+					i++;
+				}
 			}
-			i += 1;
-		}
-		while (argv[1][tmp] && (argv[1][tmp] != ' ' && argv[1][tmp] != '\t'))
-			write(1, &argv[1][tmp++], 1);
+			while (argv[1][j] && (argv[1][j] != ' ' && argv[1][j] != '\t'))
+			{
+				write (1, &argv[1][j], 1);
+				j++;
+			}
 	}
-	write(1, "\n", 1);
-	return (0);
+	write (1, "\n", 1);
+    return (0);
 }
