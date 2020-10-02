371e7889fa917be0bc4dc402a2f4822c094bc956
Dayne Mandalore <dmandalo@mi-i5.msk.21-school.ru>
2020-10-02 16:59:41 +0300

    fix

--- lvl04/rev_wstr.c
+++ lvl04/rev_wstr.c
@@ -6,42 +6,42 @@
 /*   By: dmandalo <dmandalo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/10 17:14:44 by dmandalo          #+#    #+#             */
-/*   Updated: 2020/10/01 18:01:52 by dmandalo         ###   ########.fr       */
+/*   Updated: 2020/10/02 16:58:34 by dmandalo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
- int	function (char *j, int tmp)
+int function (char *j, int tmp)
 {
-	int	i;
+	int i;
 
 	i = 0;
 	while (tmp > 0 && (j[tmp] == ' ' || j[tmp] == '\t'))
-		tmp--;
+	tmp--;
 	while (i <= tmp && j[tmp - i] != ' ' && j[tmp - i] != '\t')
-		i++;
+	i++;
 	write(1, j + tmp - i + 1, i);
 	while (tmp > 0 && (j[tmp] == ' ' || j[tmp] == '\t'))
-		tmp--;
+	tmp--;
 	if (i < tmp)
-		write(1, " ", 1);
+	write(1, " ", 1);
 	return (tmp - i);
 }
 
-int		main (int argc, char *argv[])
+int main (int argc, char *argv[])
 {
-	int	i;
+	int i;
 
+	i = 0;
 	if (argc == 2)
 	{
-		i = 0;
-		while (argv[1][i] != '\0')
+		while (argv[1][i])
 		i++;
-			i--;
+		i--;
 		while (i > 0)
-			i = function(argv[1], i);
+		i = function(argv[1], i);
 	}
 	write(1, "\n", 1);
-	return (0);
+	return(0);
 }
